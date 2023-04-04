b88ed3d5d86a36c9ab496df5c1f7f0c67e89e10b
정인상 <golsung@naver.com>
2023-04-04 16:32:14 +0900

    first commit

---
+++ deployment/kubedemo-v0/src/main/java/cloud/ez2learn/kubedemo/KubedemoApplication.java
@@ -0,0 +1,13 @@
+package cloud.ez2learn.kubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubedemoApplication.class, args);
+	}
+
+}
